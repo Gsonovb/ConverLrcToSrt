fd52675f6b3889f3c5b904b35ca0ca91360216c1
Guanyc <guanyc.dev@gmail.com>
2013-01-30 03:28:20 +0800

    首次迁入
    简单测试过第一可用版

---
+++ ConvertLrcToSrt/Module1.vb
@@ -0,0 +1,102 @@
+﻿Imports System.IO
+Imports System.Collections.Generic
+Imports System.Text
+
+Module MainModule
+
+    Sub Main()
+
+
+        Dim dir = Directory.GetCurrentDirectory()
+
+        Dim files = Directory.EnumerateFiles(dir, "*.lrc")
+
+
+
+        For Each file In files
+
+            Dim filename = Path.ChangeExtension(file, ".srt")
+
+            Convert(file, filename)
+
+        Next
+
+
+
+
+    End Sub
+
+    Dim formats As String() = {"%m\:ss\.ff", "%h\:%m\:ss\.ff"}
+    Dim dts As TimeSpan = TimeSpan.FromMilliseconds(50)
+    'Dim ssts As TimeSpan = TimeSpan.FromMilliseconds(10)
+
+    'Dim maxts As TimeSpan = TimeSpan.FromMilliseconds(250)
+
+    Dim ets As TimeSpan = TimeSpan.FromSeconds(10)
+
+
+
+    Sub Convert(inputfilename As String, outputfilename As String)
+
+        Console.WriteLine("正在转换文件{0} 到 {1} ...", inputfilename, outputfilename)
+
+        Dim sb As New StringBuilder()
+
+        Dim lines = File.ReadAllLines(inputfilename, Encoding.Default).ToList()
+
+
+
+
+        Dim srts = lines.Select(Function(s)
+                                    Dim index = lines.IndexOf(s) + 1
+                                    Dim parts = s.Split("[]".ToCharArray, StringSplitOptions.RemoveEmptyEntries)
+
+                                    Dim ts = TimeSpan.ParseExact(parts(0), formats, System.Globalization.CultureInfo.InvariantCulture)
+
+                                    Dim text = parts(1)
+
+                                    Return New With {.Index = index, .Time = ts, .Text = text}
+
+                                End Function)
+
+
+
+        '1
+        '00:00:00,000 --> 00:00:03,820
+        '使用 HTML5 设计辅助功能
+
+        Dim count = srts.Count
+
+        For i = 0 To count - 1
+
+            Dim cItem = srts(i)
+
+            Dim tts = If(i + 1 >= count,
+                         cItem.Time.Add(ets),
+                         srts(i + 1).Time.Subtract(dts))
+
+
+            sb.AppendLine(cItem.Index)
+
+            Dim stime = String.Format("{0:hh\:mm\:ss\,fff} --> {1:hh\:mm\:ss\,fff}", cItem.Time, tts)
+
+            sb.AppendLine(stime)
+
+            sb.AppendLine(cItem.Text)
+
+            sb.AppendLine()
+
+
+
+
+
+        Next
+
+
+        File.WriteAllText(outputfilename, sb.ToString)
+
+
+
+    End Sub
+
+End Module
---
+++ ConvertLrcToSrt/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' 有关程序集的常规信息通过下列特性集
+' 控制。更改这些特性值可修改
+' 与程序集关联的信息。
+
+' 查看程序集特性的值
+
+<Assembly: AssemblyTitle("ConvertLrcToSrt")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Son of VB.NET")> 
+<Assembly: AssemblyProduct("ConvertLrcToSrt")> 
+<Assembly: AssemblyCopyright("Copyright © Son of VB.NET 2013")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
+<Assembly: Guid("6c1124d0-938c-441c-b502-e6451c45461a")> 
+
+' 程序集的版本信息由下面四个值组成:
+'
+'      主版本
+'      次版本
+'      生成号
+'      修订号
+'
+' 可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
+' 方法是按如下所示使用“*”:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
